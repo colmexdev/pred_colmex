f6e8c169312a7d40da59b41b8654aa6472f04410
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-01-09 13:07:19 -0600

    Procedimiento para subir video

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'videos/id' => 'videos#id', :as => :video_id
   resources :videos
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  get 'videos/id' => 'videos#id', :as => :video_id
+
 
   # You can have the root of your site routed with "root"
   root  to: 'videos#index'
