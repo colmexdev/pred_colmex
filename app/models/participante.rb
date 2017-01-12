26d16033019bbf0ce59e8cb2aad70dee77917a96
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-01-12 17:02:38 -0600

    Procedimiento para subir video

--- app/models/participante.rb
+++ app/models/participante.rb
@@ -1,4 +1,4 @@
 class Participante < ActiveRecord::Base
   self.table_name = 'participantes'
-  attr_accesor :tipo_tipo, :sel_tipo, :tipo_curso, :sel_curso, :parts
+  attr_accessor :tipo_tipo, :sel_tipo, :tipo_curso, :sel_curso, :parts
 end
--- app/models/participante.rb~
+++ app/models/participante.rb~
@@ -1,3 +1,4 @@
 class Participante < ActiveRecord::Base
   self.table_name = 'participantes'
+  attr_accesor :tipo_tipo, :sel_tipo, :tipo_curso, :sel_curso, :parts
 end
