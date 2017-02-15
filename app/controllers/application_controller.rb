02ab4d28a6c949c32c98c8b542904f79c1e92ee5
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-02-15 17:11:46 -0600

    Integrando login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   Yt.configure do |config|
     config.api_key = 'AIzaSyDdb0WKx3fYJjfOiEGq8rF4xHTIFR82kBA'
