07770954d43ecba56682612e586cd0ca1b05ba28
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-02-16 17:41:39 -0600

    Overriding Devise controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     self.resource = warden.authenticate(auth_options)
-		logger.debug self.resource
+		logger.debug auth_options
     set_flash_message!(:notice, :signed_in)
     sign_in(resource)
     yield resource if block_given?
