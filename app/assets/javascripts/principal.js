2bb0baafbeeee8dbfeef184efb3dcc8e5d762e11
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-03-10 11:51:57 -0600

    Conglomerando peticiones AJAX

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -182,14 +182,14 @@ function scrollSelector(event, element){
 
 function scrollSelectorTouchX(event,element){
 	if(((event.changedTouches[0].clientX)/element.clientWidth) * (element.scrollWidth * 0.5) >= window.innerWidth * 0.1 )
-		element.scrollLeft = ((event.changedTouches[0].clientX )/element.clientWidth) * (element.scrollWidth * 0.1);
+		element.scrollLeft = ((event.changedTouches[0].clientX )/element.clientWidth) * (element.scrollWidth * 0.15);
 	else
 		element.scrollLeft = 0;
 }
 
 function scrollSelectorX(event, element){
 	if(((event.clientX)/element.clientWidth) * (element.scrollWidth * 0.5) >= window.innerWidth * 0.1 )
-		element.scrollLeft = ((event.clientX)/element.clientWidth) * (element.scrollWidth * 0.1);
+		element.scrollLeft = ((event.clientX)/element.clientWidth) * (element.scrollWidth * 0.15);
 	else
 		element.scrollLeft = 0;
 }
