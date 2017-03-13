9050633d57ae23a14bfbf9912a635b50c6c73b6a
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-03-13 17:10:26 -0600

    Conglomerando peticiones AJAX

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -130,16 +130,16 @@ function reproducirVideo(event,video){
 
 function scrollSelectorY(event,element,offset,touch){
 	if(touch)
-		element.scrollTop = element.scrollTop + (((event.changedTouches[0].clientY - offset) - (element.clientHeight/2)) < element.clientHeight * -0.45 ? -15 : (((event.changedTouches[0].clientY - offset) - (element.clientHeight/2)) < element.clientHeight * 0.45 ? 0 : 15 ));
+		element.scrollTop = element.scrollTop + (((event.changedTouches[0].clientY - offset) - (element.clientHeight/2)) < element.clientHeight * -0.65 ? -15 : (((event.changedTouches[0].clientY - offset) - (element.clientHeight/2)) < element.clientHeight * 0.65 ? 0 : 15 ));
 	else
-		element.scrollTop = element.scrollTop + (((event.clientY - offset) - (element.clientHeight/2)) < element.clientHeight * -0.45 ? -15 : (((event.clientY - offset) - (element.clientHeight/2)) < element.clientHeight * 0.45 ? 0 : 15 ));
+		element.scrollTop = element.scrollTop + (((event.clientY - offset) - (element.clientHeight/2)) < element.clientHeight * -0.65 ? -15 : (((event.clientY - offset) - (element.clientHeight/2)) < element.clientHeight * 0.65 ? 0 : 15 ));
 }
 
 function scrollSelectorX(event,element,offset,touch){
 	if(touch)
-		element.scrollLeft = element.scrollLeft + (((event.changedTouches[0].clientX - offset) - (element.clientWidth/2)) < element.clientWidth * -0.45 ? -15 : (((event.changedTouches[0].clientX - offset) - (element.clientWidth/2)) < element.clientWidth * 0.45 ? 0 : 15 ));
+		element.scrollLeft = element.scrollLeft + (((event.changedTouches[0].clientX - offset) - (element.clientWidth/2)) < element.clientWidth * -0.65 ? -15 : (((event.changedTouches[0].clientX - offset) - (element.clientWidth/2)) < element.clientWidth * 0.65 ? 0 : 15 ));
 	else
-		element.scrollLeft = element.scrollLeft + (((event.clientX - offset) - (element.clientWidth/2)) < element.clientWidth * -0.45 ? -15 : (((event.clientX - offset) - (element.clientWidth/2)) < element.clientWidth * 0.45 ? 0 : 15 ));
+		element.scrollLeft = element.scrollLeft + (((event.clientX - offset) - (element.clientWidth/2)) < element.clientWidth * -0.65 ? -15 : (((event.clientX - offset) - (element.clientWidth/2)) < element.clientWidth * 0.65 ? 0 : 15 ));
 }
 
 function cambiarGif(event,gif){
