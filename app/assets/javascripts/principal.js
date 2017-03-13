353eef079fb9ff41595db6b922e6a6f98c2e6f3f
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-03-13 11:15:31 -0600

    Conglomerando peticiones AJAX

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -135,14 +135,14 @@ function scrollSelector(event, element){
 
 function scrollSelectorTouchX(event,element){
 	//if(((event.changedTouches[0].clientX)/element.clientWidth) * (element.scrollWidth * 0.5) >= window.innerWidth * 0.1 )
-		element.scrollLeft = element.ScrollLeft + (event.changedTouches[0].clientX - (element.clientWidth/2));/*((event.changedTouches[0].clientX )/element.clientWidth) * (element.scrollWidth * 0.15);*/
+		element.scrollLeft = element.ScrollLeft + (event.changedTouches[0].clientX - (element.clientWidth/2))*0.5;/*((event.changedTouches[0].clientX )/element.clientWidth) * (element.scrollWidth * 0.15);*/
 	//else
 	//	element.scrollLeft = 0;
 }
 
 function scrollSelectorX(event, element){
 	//if(((event.clientX)/element.clientWidth) * (element.scrollWidth * 0.5) >= window.innerWidth * 0.1 )
-		element.scrollLeft = element.scrollLeft + (event.clientX - (element.clientWidth/2)); /*((event.clientX)/element.clientWidth) * (element.scrollWidth * 0.15);*/
+		element.scrollLeft = element.scrollLeft + (event.clientX - (element.clientWidth/2))*0.5; /*((event.clientX)/element.clientWidth) * (element.scrollWidth * 0.15);*/
 	//else
 	//	element.scrollLeft = 0;
 }
