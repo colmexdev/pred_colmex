3dc77a33310560f46834e98f1fd13aeb0351eb4d
RickS1 <sgt_pepper_lonely_1822@hotmail.com>
2017-03-13 17:03:59 -0600

    Conglomerando peticiones AJAX

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -130,16 +130,16 @@ function reproducirVideo(event,video){
 
 function scrollSelectorY(event,element,offset,touch){
 	if(touch)
-		element.scrollTop = element.scrollTop + (((event.changedTouches[0].clientY - offset) - (element.clientHeight / 2)) < 0 ? -25 : 25 );
+		element.scrollTop = element.scrollTop + (((event.changedTouches[0].clientY - offset) - (element.clientHeight/2)) < window.innerHeight * -0.45 ? -15 : (((event.changedTouches[0].clientY - offset) - (element.clientHeight/2)) < window.innerHeight * 0.45 ? 0 : 15 ));
 	else
-		element.scrollTop = element.scrollTop + (((event.clientY - offset) - (element.clientHeight / 2)) < 0 ? -25 : 25 );
+		element.scrollTop = element.scrollTop + (((event.clientY - offset) - (element.clientHeight/2)) < window.innerHeight * -0.45 ? -15 : (((event.clientY - offset) - (element.clientHeight/2)) < window.innerHeight * 0.45 ? 0 : 15 ));
 }
 
 function scrollSelectorX(event,element,offset,touch){
 	if(touch)
-		element.scrollLeft = element.scrollLeft + (((event.changedTouches[0].clientX - offset) - (element.clientWidth / 2)) < 0 ? -25 : 25 );
+		element.scrollLeft = element.scrollLeft + (((event.changedTouches[0].clientX - offset) - (element.clientWidth/2)) < window.innerWidth * -0.45 ? -15 : (((event.changedTouches[0].clientX - offset) - (element.clientWidth/2)) < window.innerWidth * 0.45 ? 0 : 15 ));
 	else
-		element.scrollLeft = element.scrollLeft + (((event.clientX - offset) - (element.clientWidth / 2)) < 0 ? -25 : 25 );
+		element.scrollLeft = element.scrollLeft + (((event.clientX - offset) - (element.clientWidth/2)) < window.innerWidth * -0.45 ? -15 : (((event.clientX - offset) - (element.clientWidth/2)) < window.innerWidth * 0.45 ? 0 : 15 ));
 }
 
 function cambiarGif(event,gif){
