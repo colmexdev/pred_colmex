ebc899b5422a7599bacb0799043897d60c0006ce
colmexdev <github@colmex.mx>
2018-04-04 17:24:14 -0500

    Escalado de logo

--- app/assets/javascripts/principal.js
+++ app/assets/javascripts/principal.js
@@ -89,9 +89,9 @@ function slide_pagina(event,render,liga,home){
 			$("#mosaico").css("display","block");
 	});
 
-	$("#brand").animate({width: (/http:\/\/pred1\.colmex\.mx\/?$/.test(window.location.href) ? "130px" : "65px"), height: (/http:\/\/pred1\.colmex\.mx\/?$/.test(window.location.href) ? "100px" : "50px")}, 50);
-
 	window.history.pushState({},"Nueva página",liga);
+
+	$("#brand").animate({width: (/http:\/\/pred1\.colmex\.mx\/?$/.test(window.location.href) ? "130px" : "65px"), height: (/http:\/\/pred1\.colmex\.mx\/?$/.test(window.location.href) ? "100px" : "50px")}, 50);
 
 	setTimeout(function(){
 		$("#brand,#menu_canvas,#menu_mask").animate({top: (!(/http:\/\/pred1\.colmex\.mx\/?$/.test(window.location.href)) ? "1%" : "17%")}, 90);
