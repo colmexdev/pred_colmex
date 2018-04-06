36a0d44932941e1134cf968146e34bd014ac939f
colmexdev <github@colmex.mx>
2018-04-06 17:03:57 -0500

    Tablas listas

--- db/migrate/20180406220143_create_playlist.rb
+++ db/migrate/20180406220143_create_playlist.rb
@@ -2,6 +2,7 @@ class CreatePlaylist < ActiveRecord::Migration
   def change
     create_table :playlists do |t|
       t.text :nombre, null: false
+      t.timestamps null: false
     end
   end
 end
---
+++ db/migrate/20180406220233_create_curso_breve.rb
@@ -0,0 +1,8 @@
+class CreateCursoBreve < ActiveRecord::Migration
+  def change
+    create_table :cursos_breves do |t|
+      t.text :nombre, null: false
+      t.timestamps null false
+    end
+  end
+end
