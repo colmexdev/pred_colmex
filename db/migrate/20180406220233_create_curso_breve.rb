9f7864c66df3e375667a3bcf5b2be0a509f4a4e1
colmexdev <github@colmex.mx>
2018-04-06 17:07:35 -0500

    Tablas listas

--- db/migrate/20180406220233_create_curso_breve.rb
+++ db/migrate/20180406220233_create_curso_breve.rb
@@ -2,7 +2,7 @@ class CreateCursoBreve < ActiveRecord::Migration
   def change
     create_table :cursos_breves do |t|
       t.text :nombre, null: false
-      t.timestamps null false
+      t.timestamps null: false
     end
   end
 end
