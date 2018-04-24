5ec47130d634318e768a7e34d5ba0e07b4cd5833
colmexdev <github@colmex.mx>
2018-04-24 17:48:58 -0500

    Actualizar  videos por id

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -43,7 +43,6 @@ class PanelController < ApplicationController
       config.log_level = :debug
       config.client_id = Rails.application.secrets.yt_client
       config.client_secret = Rails.application.secrets.yt_secret
-      config.api_key = Rails.application.secrets.api_key
     end
     @acc = Yt::Account.new refresh_token: Rails.application.secrets.yt_token
     if params[:keyword].present?
